0617f6fd6c27eab18d38841fdbd51dbe4f81e57d
patrick thornton <patrickthornton@college.harvard.edu>
2024-02-08 23:24:58 -0500

    docs

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,10 @@ import adapter from "@sveltejs/adapter-static";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
+    appDir: "app",
     adapter: adapter({
+      pages: "docs",
+      assets: "docs",
       fallback: "404.html",
     }),
   },
